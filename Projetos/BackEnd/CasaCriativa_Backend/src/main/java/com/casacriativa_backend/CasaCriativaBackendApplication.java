140f806d8735818ecd3d69253076a3c915418f77
Humberto Munhoz <106282958+HMDSilva@users.noreply.github.com>
2023-04-05 22:31:31 -0300

    Criação do backend
    Criado projeto maven com jpa, postgres e springboot.

---
+++ Projetos/BackEnd/CasaCriativa_Backend/src/main/java/com/casacriativa_backend/CasaCriativaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.casacriativa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaCriativaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasaCriativaBackendApplication.class, args);
+    }
+
+}
---
+++ Projetos/BackEnd/CasaCriativa_Backend/src/test/java/com/casacriativa_backend/CasaCriativaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casacriativa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaCriativaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
