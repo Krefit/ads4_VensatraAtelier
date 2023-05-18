36dd6c339f330adcd340542781abf90fc9a630a2
Humberto Munhoz <106282958+HMDSilva@users.noreply.github.com>
2023-05-18 17:14:16 -0300

    Implementação da tela  fornecedores

--- Projetos/FrontEnd/sidenav-with-multilevel-menu/src/app/app.module.ts
+++ Projetos/FrontEnd/sidenav-with-multilevel-menu/src/app/app.module.ts
@@ -3,21 +3,37 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< Updated upstream
 import { MatFormFieldModule } from '@angular/material/form-field';
+=======
+>>>>>>> Stashed changes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { PagesComponent } from './cliente/cliente.component';
 import { SettingsComponent } from './configuracoes/configuracoes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { FornecedoresComponent } from './fornecedores/fornecedores.component';
+import { FornecedoresListComponent } from './fornecedores/fornecedores-list/fornecedores-list.component';
 import { StatisticsComponent } from './material/material.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { SublevelMenuComponent } from './sidenav/sublevel-menu.component';
+<<<<<<< Updated upstream
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CoreServiceComponent } from './core-service/core-service.component';
 
+=======
+import {MatInputModule} from "@angular/material/input";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { FornecedorEditarComponent } from './fornecedores/fornecedor-editar/fornecedor-editar.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatCardModule} from "@angular/material/card";
+>>>>>>> Stashed changes
 
 
 @NgModule({
@@ -28,12 +44,16 @@ import { CoreServiceComponent } from './core-service/core-service.component';
     DashboardComponent,
     StatisticsComponent,
     PagesComponent,
-    FornecedoresComponent,
+    FornecedoresListComponent,
     SettingsComponent,
     SublevelMenuComponent,
+<<<<<<< Updated upstream
     CoreServiceComponent,
 
 
+=======
+    FornecedorEditarComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
@@ -41,12 +61,24 @@ import { CoreServiceComponent } from './core-service/core-service.component';
     AppRoutingModule,
     HttpClientModule,
     MatTableModule,
+<<<<<<< Updated upstream
     MatFormFieldModule,
     FormsModule,
 
 
 
 
+=======
+    MatInputModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatCardModule
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent,CommonModule],
---
+++ Projetos/FrontEnd/sidenav-with-multilevel-menu/src/app/fornecedores/fornecedores-list/fornecedores-list.component.ts
@@ -0,0 +1,98 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatDialog} from "@angular/material/dialog";
+import {FornecedoresService} from "../../services/fornecedores.service";
+import {CoreService} from "../../services/core.service";
+import {FornecedorEditarComponent} from "../fornecedor-editar/fornecedor-editar.component";
+
+@Component({
+  selector: 'app-media',
+  templateUrl: './fornecedores-list.component.html',
+  styleUrls: ['./fornecedores-list.component.scss']
+})
+export class FornecedoresListComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'nome',
+    'cnpj',
+    'endereco',
+    'telefone',
+    'estado',
+    'cidade',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor(
+    private _dialog: MatDialog,
+    private _fornService: FornecedoresService,
+    private _coreService: CoreService
+
+
+  ) {}
+
+  ngOnInit(): void {
+    this.getFornecedorListar();
+  }
+
+  openAddEditEmpForm() {
+    const dialogRef = this._dialog.open(FornecedorEditarComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getFornecedorListar();
+        }
+      },
+    });
+  }
+
+  getFornecedorListar() {
+    this._fornService.getFornecedorList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteFornecedor(id: number) {
+    this._fornService.deleteFornecedor(id).subscribe({
+      next: (res) => {
+        this._coreService.openSnackBar('Employee deleted!', 'done');
+        this.getFornecedorListar();
+      },
+      error: console.log,
+    });
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(FornecedorEditarComponent, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getFornecedorListar();
+        }
+      },
+    });
+  }
+}
