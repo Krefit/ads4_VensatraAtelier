a672be887ff7cc55eced760b8cb48ce944d88cdd
Krefit <gustavokrawczyk@gmail.com>
2023-06-01 21:13:05 -0300

    Atualização de uma caca feita por mim

--- Projetos/BackEnd/CasaCriativa_Backend/src/main/java/com/casacriativa_backend/CasaCriativaBackendApplication.java
+++ Projetos/BackEnd/CasaCriativa_Backend/src/main/java/com/casacriativa_backend/CasaCriativaBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CasaCriativaBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CasaCriativaBacke,0ndApplication.class, args);
+        SpringApplication.run(CasaCriativaBackendApplication.class, args);
     }
 
 }
