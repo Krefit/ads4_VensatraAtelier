309722913d20c6607ecde623adaac456a418ae40
LetyciaEst08 <letyciaesteves@hotmail.com>
2023-06-17 17:53:43 -0300

    adicionando authguard nas rotas, bloqueando o acesso aos componentes

--- Projetos/FrontEnd/sidenav-with-multilevel-menu/src/app/app-routing.module.ts
+++ Projetos/FrontEnd/sidenav-with-multilevel-menu/src/app/app-routing.module.ts
@@ -7,36 +7,77 @@ import { SettingsComponent } from './configuracoes/configuracoes.component';
 import { StatisticsComponent } from './material/material.component';
 import { OrcamentosComponent } from './orcamento/orcamento.component';
 import { LoginComponent } from './login/login/login.component';
+import { AuthGuard } from './login/login/authGuard';
 
 
 
-const routes: Routes = [
-    {
-    path: '', pathMatch: 'full',
-    redirectTo: 'login'
-    }
-    ,
-    {
-      path: 'login', component: LoginComponent
-    },
+    const routes: Routes = [
+
+      {
+        path: '', pathMatch: 'full',
+        redirectTo: 'login'
+        }
+        ,
+        {
+          path: 'login', component: LoginComponent
+        },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [AuthGuard]
+      },
+
+      {
+        path: 'produtos',
+        loadChildren: () => import('./produtos/produtos.module').then(m => m.ProductsModule),
+        canActivate: [AuthGuard]
+      },
+
+      {
+        path: 'material',
+        component: StatisticsComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'orcamento',
+        component: OrcamentosComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'cliente',
+        component: PagesComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'fornecedores',
+        component: FornecedoresListComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'configuracoes',
+        component: SettingsComponent,
+        canActivate: [AuthGuard]
+      }
+    // ];
 
 
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
-  {
-    path: 'produtos',
-    loadChildren: () => import('./produtos/produtos.module').then(m => m.ProductsModule)
-  },
-  {path: 'material', component: StatisticsComponent},
-  {path: 'orcamento', component: OrcamentosComponent},
-  {path: 'cliente', component: PagesComponent},
-  {path: 'fornecedores', component: FornecedoresListComponent},
-  {path: 'configuracoes', component: SettingsComponent}
+  // {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  // {path: 'dashboard', component: DashboardComponent},
+  // {
+  //   path: 'produtos',
+  //   loadChildren: () => import('./produtos/produtos.module').then(m => m.ProductsModule)
+  // },
+  // {path: 'material', component: StatisticsComponent},
+  // {path: 'orcamento', component: OrcamentosComponent},
+  // {path: 'cliente', component: PagesComponent},
+  // {path: 'fornecedores', component: FornecedoresListComponent},
+  // {path: 'configuracoes', component: SettingsComponent}
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ Projetos/FrontEnd/sidenav-with-multilevel-menu/src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+  private isAuthenticatedValue: boolean = false;
+
+  constructor() { }
+
+  login(username: string, password: string): boolean {
+    // Lógica para autenticar o usuário (por exemplo, fazer uma solicitação HTTP para um servidor)
+
+    // Exemplo de lógica de autenticação simples para demonstração
+    if (username === 'admin' && password === 'admin') {
+      this.isAuthenticatedValue = true;
+      return true;
+    } else {
+      this.isAuthenticatedValue = false;
+      return false;
+    }
+  }
+
+  logout(): void {
+    this.isAuthenticatedValue = false;
+  }
+
+  isAuthenticated(): boolean {
+    return this.isAuthenticatedValue;
+  }
+}
